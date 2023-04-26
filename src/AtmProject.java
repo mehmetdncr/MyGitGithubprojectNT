e3b4d575a2bb04b79eddd04e39037535fc4598bb
mehmetdncr <dincer45@gmail.com>
2023-04-26 15:00:43 -0400

    v2

--- src/AtmProject.java
+++ src/AtmProject.java
@@ -2,6 +2,8 @@ public class AtmProject {
     public static void main(String[] args) {
         System.out.println("Bu bir ATM projesidir");
         //version1
+        System.out.println("Java is easy oldurur");
+        //version2
 
     }
 }
